0a3d30aa2100713e59a2dc2da34b085c55159c6c
Mohammad Hanif Furqan Aufa Putra <m.hanif.furqan@gmail.com>
2025-05-25 09:06:52 +0700

    Task-03

--- progjar-task-03/progjar4a/file_protocol.py
+++ progjar-task-03/progjar4a/file_protocol.py
@@ -21,17 +21,18 @@ string
 class FileProtocol:
     def __init__(self):
         self.file = FileInterface()
-    def proses_string(self,string_datamasuk=''):
+
+    def proses_string(self, string_datamasuk=''):
         logging.warning(f"string diproses: {string_datamasuk}")
-        c = shlex.split(string_datamasuk.lower())
         try:
-            c_request = c[0].strip()
+            c = shlex.split(string_datamasuk)
+            c_request = c[0].lower().strip()
             logging.warning(f"memproses request: {c_request}")
-            params = [x for x in c[1:]]
-            cl = getattr(self.file,c_request)(params)
+            params = c[1:]
+            cl = getattr(self.file, c_request)(params)
             return json.dumps(cl)
         except Exception:
-            return json.dumps(dict(status='ERROR',data='request tidak dikenali'))
+            return json.dumps(dict(status='ERROR', data='request tidak dikenali'))
 
 
 if __name__=='__main__':
